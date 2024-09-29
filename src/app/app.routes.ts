a819cda6d0676d382bbe784a61208fe66700f79a
rangelovvjobs <rangelovvjobs@gmail.com>
2024-09-29 18:43:22 +0300

    simple routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./features/authentication/authentication.component').then(
+        (c) => c.AuthenticationComponent
+      ),
+  },
+];
