b99dcb58cbd7e99f34a1f12245a5b916318bcaa1
rangelovvjobs <rangelovvjobs@gmail.com>
2024-09-30 12:51:04 +0300

    basic layout and styling of the AddActivity component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserPanelComponent } from '../features/user-panel/user-panel.component';
+import { AddActivityPanelComponent } from '../features/add-activity-panel/add-activity-panel.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [UserPanelComponent],
+  imports: [UserPanelComponent, AddActivityPanelComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
