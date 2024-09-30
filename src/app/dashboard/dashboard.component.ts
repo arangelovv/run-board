3d338b24a71762133292db3c25f1b7ae8ba167b0
rangelovvjobs <rangelovvjobs@gmail.com>
2024-09-30 13:28:28 +0300

    basic layout of the app and its core components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { UserPanelComponent } from '../features/user-panel/user-panel.component';
 import { AddActivityPanelComponent } from '../features/add-activity-panel/add-activity-panel.component';
-
+import { ActivitiesTableComponent } from '../features/activities-table/activities-table.component';
+import { StatisticsPanelComponent } from '../features/statistics-panel/statistics-panel.component';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [UserPanelComponent, AddActivityPanelComponent],
+  imports: [
+    UserPanelComponent,
+    AddActivityPanelComponent,
+    ActivitiesTableComponent,
+    StatisticsPanelComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
---
+++ src/app/features/statistics-panel/statistics-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './statistics-panel.component.html',
+  styleUrl: './statistics-panel.component.scss'
+})
+export class StatisticsPanelComponent {
+
+}
