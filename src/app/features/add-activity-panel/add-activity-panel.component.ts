3f3531ac5dea82f5f12781cad62067ac44b05a90
rangelovvjobs <rangelovvjobs@gmail.com>
2024-09-30 12:39:59 +0300

    created some feature components

---
+++ src/app/features/add-activity-panel/add-activity-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-activity-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-activity-panel.component.html',
+  styleUrl: './add-activity-panel.component.scss'
+})
+export class AddActivityPanelComponent {
+
+}
---
+++ src/app/features/statistics-table/statistics-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './statistics-table.component.html',
+  styleUrl: './statistics-table.component.scss'
+})
+export class StatisticsTableComponent {
+
+}
